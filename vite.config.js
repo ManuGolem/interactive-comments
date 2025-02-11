bfbe3e84d42592b2b7700e7515a5ca528e452b02
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-10 21:59:08 -0300

    remove base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -10,6 +9,5 @@ export default defineConfig({
   ],
   build:{
     outDir:"docs"
-  },
-  base:"interactive-comments"
+  }
 })
