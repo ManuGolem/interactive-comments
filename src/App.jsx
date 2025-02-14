40555c163c5909e88ed8b73dc8456fe368a35fc6
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-13 22:58:20 -0300

    fetch to json archive local

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { Comentario } from "./Comentario"
-export function App(){
-    const {img,nombre,fecha,texto}=0;
-    return(
+import { Comentario } from "./Comentario";
+export function App() {
+    traerDatos();
+    async function traerDatos() {
+        const data = await fetch("data.json").then((response) =>
+            response.json(),
+        );
+        const { comments, currentUser } = data;
+        console.log(comments, currentUser);
+        comments.forEach((e) => {
+            console.log(e.id, e.content, e.createdAt, e.user);
+        });
+    }
+
+    const { img, nombre, fecha, texto } = 0;
+    return (
         <section className="bg-very-light-gray">
             <Comentario png={img} name={nombre} date={fecha} text={texto} />
-            <Comentario/>
-            <Comentario/>
-            <Comentario/>
-            <Comentario/>
+            <Comentario png={img} name={nombre} date={fecha} text={texto} />
+            <Comentario png={img} name={nombre} date={fecha} text={texto} />
+            <Comentario png={img} name={nombre} date={fecha} text={texto} />
         </section>
-    )
+    );
 }
