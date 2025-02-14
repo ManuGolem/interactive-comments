280087f90544a7e3fa30efa897bd41bcca8c3529
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-14 14:49:51 -0300

    add some class

--- src/Comentario.jsx
+++ src/Comentario.jsx
@@ -1,14 +1,19 @@
-export function Comentario({png,name,date,text}){
-    return(
-        <div className="w-[50%] bg-white mb-1 ml-auto mr-auto">
-            {/* <BotonPuntuacion/> */}
-            <header>
-                <img src={png}></img>
-                <h2>{name}</h2>
-                <h2>{date}</h2>
-                <button>Reply</button>
-                <p>{text}</p>
-            </header>
+import { BotonPuntuacion } from "./BotonPuntacion";
+export function Comentario({ img, name, date, text }) {
+    return (
+        <div className="flex w-[50%] bg-white mb-1 ml-auto mr-auto p-5 gap-5 rounded-[8px] mt-8">
+            <BotonPuntuacion score="12" />
+            <div>
+                <header className="flex gap-4 items-center mb-5">
+                    <img src={img} className="w-[32px]"></img>
+                    <h2 className="font-bold">{name}</h2>
+                    <p className="text-grayish-blue">{date}</p>
+                    <button className="ml-auto hover:cursor-pointer">
+                        Reply
+                    </button>
+                </header>
+                <p className="text-grayish-blue text-start">{text}</p>
+            </div>
         </div>
-    )
+    );
 }
