992a93fde386368bfa12ada2e064aa282a726ec8
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-14 23:45:59 -0300

    add the replies of the comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,62 @@ import { Comentario } from "./Comentario";
 import { useState } from "react";
 export function App() {
     const [comentarios, setComentarios] = useState([]);
-    console.log(comentarios.length);
     useEffect(() => {
         async function traerDatos() {
             const data = await fetch("data.json").then((response) =>
                 response.json(),
             );
             setComentarios(data.comments);
+            console.log(data.comments[1].replies);
         }
         traerDatos();
     }, []);
     return (
-        <section className="bg-very-light-gray">
+        <section>
             {comentarios.length > 0 ? (
-                comentarios.map((comentario) => (
-                    <Comentario
-                        key={comentario.id}
-                        img={comentario.user.image.webp}
-                        name={comentario.user.username}
-                        date={comentario.createdAt}
-                        text={comentario.content}
-                    />
-                ))
+                comentarios.map((comentario) =>
+                    comentario.replies.length > 0 ? (
+                        <div
+                            key={comentario.id}
+                            className="flex flex-col justify-center"
+                        >
+                            <Comentario
+                                img={comentario.user.image.webp}
+                                name={comentario.user.username}
+                                date={comentario.createdAt}
+                                text={comentario.content}
+                                tipo="Comentario"
+                            />
+                            <div className="respuesta">
+                                <div className="respuesta-sep">
+                                    {comentario.replies.map((respuesta) => (
+                                        <Comentario
+                                            key={respuesta.id}
+                                            img={respuesta.user.image.webp}
+                                            name={respuesta.user.username}
+                                            date={respuesta.createdAt}
+                                            text={"@".concat(
+                                                respuesta.replyingTo.concat(
+                                                    respuesta.content,
+                                                ),
+                                            )}
+                                            tipo="Respuesta"
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <Comentario
+                            key={comentario.id}
+                            img={comentario.user.image.webp}
+                            name={comentario.user.username}
+                            date={comentario.createdAt}
+                            text={comentario.content}
+                            tipo="Comentario"
+                        />
+                    ),
+                )
             ) : (
                 <p>No hay comentarios Disponibles</p>
             )}
