a085ff282b3e6e814f9042e498c75873afcb64c5
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-15 00:04:17 -0300

    add two types of comments 1.Comments pure 2.Replies

--- src/Comentario.jsx
+++ src/Comentario.jsx
@@ -1,14 +1,7 @@
 import { BotonPuntuacion } from "./BotonPuntacion";
-export function Comentario({ img, name, date, text, tipo }) {
-    let clases;
-    if (tipo == "Comentario") {
-        clases =
-            "flex w-[50%] bg-white mb-1 ml-auto mr-auto p-5 gap-5 rounded-[8px] mt-8";
-    } else {
-        clases = "flex bg-white p-5 gap-5 rounded-[8px]";
-    }
-    return (
-        <div className={clases}>
+export function Comentario({ img, name, date, text, tipo, responseTo }) {
+    return tipo === "Comentario" ? (
+        <div className="flex w-[50%] bg-white mb-1 ml-auto mr-auto p-5 gap-5 rounded-[8px] mt-8">
             <BotonPuntuacion score="12" />
             <div>
                 <header className="flex gap-4 items-center mb-5">
@@ -20,6 +13,26 @@ export function Comentario({ img, name, date, text, tipo }) {
                     </button>
                 </header>
                 <p className="text-grayish-blue text-start">{text}</p>
+            </div>
+        </div>
+    ) : (
+        <div className="flex bg-white p-5 gap-5 rounded-[8px]">
+            <BotonPuntuacion score="12" />
+            <div>
+                <header className="flex gap-4 items-center mb-5">
+                    <img src={img} className="w-[32px]"></img>
+                    <h2 className="font-bold">{name}</h2>
+                    <p className="text-grayish-blue">{date}</p>
+                    <button className="ml-auto hover:cursor-pointer">
+                        Reply
+                    </button>
+                </header>
+                <p className="text-grayish-blue text-start">
+                    <strong className="text-moderate-blue font-bold">
+                        {responseTo}
+                    </strong>{" "}
+                    {text}
+                </p>
             </div>
         </div>
     );
