6aa3c002bfb44f479366d7f2267803a8f8c734eb
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-16 20:33:38 -0300

    add the send text value of textarea

--- src/Comentar.jsx
+++ src/Comentar.jsx
@@ -1,14 +1,28 @@
-export function Comentar({ img, username }) {
+import { useState } from "react";
+export function Comentar({ img, username, funcion }) {
+    const [texto, setTexto] = useState("");
+    function guardarTexto(e) {
+        setTexto(e.target.value);
+    }
+    function enviar() {
+        funcion(texto);
+        setTexto("");
+    }
     return (
         <article className="w-[50%] mr-auto ml-auto flex bg-white p-5 gap-5 rounded-[8px] items-start">
             <header className="flex gap-4 items-center mb-5">
                 <img src={img} className="w-[40px]"></img>
             </header>
             <textarea
+                onChange={guardarTexto}
                 className="bg-white resize-none rounded-2xl w-full border-2 border-light-gray px-5 py-2 h-[6rem] focus-visible:outline-none"
                 placeholder="Add a comment..."
+                value={texto}
             ></textarea>
-            <button className="bg-moderate-blue px-5 py-2 text-white rounded-[8px] hover:bg-light-grayish-blue hover:cursor-pointer">
+            <button
+                onClick={enviar}
+                className="bg-moderate-blue px-5 py-2 text-white rounded-[8px] hover:bg-light-grayish-blue hover:cursor-pointer"
+            >
                 SEND
             </button>
         </article>
