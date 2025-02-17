604e9bff5ffd5da3a2e593889dd9671c0c507353
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-17 18:06:59 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  build:{
-    outDir:"docs"
-  }
-})
+    plugins: [react(), tailwindcss()],
+    build: {
+        outDir: "docs",
+    },
+    base: "interactive-comments",
+});
