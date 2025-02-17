d863504d086073308605a1aecb7bba32f191876c
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-17 13:18:43 -0300

    component for the header of comments to reduce large code

---
+++ src/HeaderComment.jsx
@@ -0,0 +1,30 @@
+export function HeaderComment({ img, date, name, currentUser }) {
+    return (
+        <header className="flex gap-4 items-center mb-5 justify-end">
+            <img src={img} className="w-[32px]"></img>
+            <h2 className="font-bold">{name}</h2>
+            {currentUser != null ? (
+                currentUser == name ? (
+                    <p className="bg-moderate-blue text-white px-2 "> you</p>
+                ) : null
+            ) : null}
+            <p className="text-grayish-blue text-start grow">{date}</p>
+            {currentUser != null ? (
+                currentUser == name ? (
+                    <button className=" text-soft-red flex gap-1 items-center hover:cursor-pointer">
+                        <img src="images/icon-delete.svg" />
+                        Delete
+                    </button>
+                ) : null
+            ) : null}
+
+            <button className="hover:cursor-pointer flex gap-1 items-center text-moderate-blue font-bold">
+                <img
+                    className="w-[14px] h-[13px]"
+                    src="images/icon-reply.svg"
+                />
+                Reply
+            </button>
+        </header>
+    );
+}
