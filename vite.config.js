05688cfbde4352f1f37919d25a4c457511ba135a
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-17 23:45:24 -0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
+export default defineConfig(({ mode } ) =>({
     plugins: [react(), tailwindcss()],
     build: {
         outDir: "docs",
     },
-    base: "interactive-comments",
-});
+    base: mode === "production" ? "interactive-comments" : "/",
+}));
